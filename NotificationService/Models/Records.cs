5fca0b0079b644d1e89ecf8622db7b894d8b4371
Your Name <you@example.com>
2026-01-06 17:11:48 +0200

    Refactor message handler config into a record type
    Encapsulate the message handler dictionary in a new MessageHandlresConfig record for improved structure and type safety. Update Program.cs to use this record, enhancing maintainability and code clarity.

---
+++ NotificationService/Models/Records.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotificationService.Models
+{
+    public record MessageHandlresConfig(Dictionary<string, Type> messageHandlers);
+}
--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -1,28 +1,26 @@
-using Microsoft.Extensions.DependencyInjection;
 using NotificationService.Models;
 using NotificationService.Services;
 using NotificationService.Services.MessageHandlers;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Register Kafka consumer service
 builder.Services.AddSingleton<IFcmService, FcmService>();
 builder.Services.AddSingleton<IMessageDispatcher, MessageDispatcher>();
 builder.Services.AddSingleton<IKafkaConsumer, KafkaConsumer>();
 
-var messageHandlers = new Dictionary<string, Type>
+var messageHandlersConfig = new MessageHandlresConfig(new Dictionary<string, Type>
 {
     { "audio.analyze.completed", typeof(AudioAnalysisCompletedMessagaHandler) },
     { "user.registered", typeof(UserRegisteredMessagaHandler) },
     { "user.deregistered", typeof(UserDeregisteredMessagaHandler) }
-};
+});
 
-foreach (var kvp in messageHandlers)
+foreach (var kvp in messageHandlersConfig.messageHandlers)
 {
     builder.Services.AddKeyedSingleton(typeof(IMessageHandler), kvp.Key, kvp.Value);
 }
 
-builder.Services.AddSingleton(new TopicConfiguration(messageHandlers.Keys.ToArray()));
+builder.Services.AddSingleton(new TopicConfiguration(messageHandlersConfig.messageHandlers.Keys.ToArray()));
 
 builder.Services.AddHostedService<Worker>();
 
